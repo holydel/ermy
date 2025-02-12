51a7ddc618d15993c0d72ab0c73ddff301822bf9
holydel <holydel@gmail.com>
2025-02-12 19:08:30 +0500

    shaders

--- editor/include/assets_preview_manager.h
+++ editor/include/assets_preview_manager.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <ermy_api.h>
+
+namespace editor
+{
+	class PreviewManager
+	{
+		PreviewManager();
+		~PreviewManager();
+	public:
+		PreviewManager& Instance();
+
+		int GetPreviewIconSize();
+		int Get
+	};
+}
