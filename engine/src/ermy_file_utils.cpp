d198853f6427ad4480e3890021cdf577e250ce23
holydel <holydel@gmail.com>
2025-03-31 00:33:45 +0500

    bad ai suggestions

--- engine/src/ermy_file_utils.cpp
+++ engine/src/ermy_file_utils.cpp
@@ -9,16 +9,11 @@ std::vector<ermy::u8> ermy_utils::file::ReadFile(const std::filesystem::path& pa
 		return std::vector<ermy::u8>();
 	}
 
-	// Disable synchronization with C-style I/O for better performance
-	std::ios::sync_with_stdio(false);
-
 	file.seekg(0, std::ios::end);
 	size_t size = file.tellg();
 	file.seekg(0, std::ios::beg);
 
-	std::vector<ermy::u8> data;
-	data.reserve(size);  // Pre-allocate to avoid reallocations
-	data.resize(size);   // Set the actual size
+	std::vector<ermy::u8> data(size);
 	file.read(reinterpret_cast<char*>(data.data()), size);
 
 	return data;
@@ -31,9 +26,6 @@ bool ermy_utils::file::WriteFile(const std::filesystem::path& path, const std::v
 		return false;
 	}
 
-	// Disable synchronization with C-style I/O for better performance
-	std::ios::sync_with_stdio(false);
-
 	file.write(reinterpret_cast<const char*>(data.data()), data.size());
-	return file.good();  // Return success status
+	return file.good();
 }
